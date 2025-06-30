cff28652812f5830756d254d3890b9b12a831d5a
mywind <ryviuszero@gmail.com>
2025-06-30 15:55:02 +0800

    finish for 01 challenges

---
+++ answers/chapter01_introduction/a02/Hello.java
@@ -0,0 +1,9 @@
+package answers.chapter01_introduction.a02;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+    }
+}
