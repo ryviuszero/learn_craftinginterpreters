38f72043318d50afbd3958bee375d79d8a140525
mywind <ryviuszero@gmail.com>
2025-07-04 17:33:24 +0800

    first commit for scanning

---
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,73 @@
+package java.com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import java.com.craftinginterpreters.lox.Scanner;
+
+
+public class Lox {
+
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+
+    }
+
+    private static void runFile(String path) throws IOException {
+        // Implement file execution logic here
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) {
+            System.exit(65);
+        }
+    }
+
+    private static void runPrompt()  throws IOException {
+        // Implement REPL logic here
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false; // Reset error state for the next line
+        }
+    }
+
+    private static void run(String source) {
+        // Implement source code execution logic here
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token: tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
+
+
